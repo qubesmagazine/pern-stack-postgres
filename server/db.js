d56cce689415e786b16b9834ec951de78e06f928
Onyido Okwudili <122439993+qubesmagazine@users.noreply.github.com>
2024-04-08 09:56:54 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,10 +1,10 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-user: "postgres",
-password: "oyindoo1",
+user: "",
+password: "",
 port: 5432,
-database: "perntodo"
+database: ""
 });
 
 module.exports = pool;
